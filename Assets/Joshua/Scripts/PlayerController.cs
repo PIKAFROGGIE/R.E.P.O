07153d80c55097619d7ab8c7a53f43dd80d70e08
PIKACHU <pikaffroggie@gmail.com>
2025-12-28 17:37:10 +0800

    push box anim

--- Assets/Joshua/Scripts/PlayerController.cs
+++ Assets/Joshua/Scripts/PlayerController.cs
@@ -32,16 +32,24 @@ public class PlayerController : MonoBehaviour
     public bool canWalk = true, canRun = true, canJump = true, stopSound = false, isPause = false;
 
 
-    //public GameObject bullet;
 
     public int layerIndex = 0;
 
     public float longPressDuration = 1f; // Duration to consider as a long press
 
-    public float fireRate = 0.1f;        // Time between shots
-    public float bulletSpeed = 20f;
-    public bool isShooting = false;
     PhotonView PV;
+
+    [Header("Combo Settings")]
+    public float comboInputWindow = 0.5f;
+    public int totalComboCount = 2;
+
+    [Header("References")]
+    //public DamageObject[] punchDamages;
+
+    private int comboIndex = 0;
+    private bool isAttacking = false;
+    private bool inputBuffered = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -91,7 +99,7 @@ public class PlayerController : MonoBehaviour
 
         CC.Move(moveInput * Time.deltaTime);
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
-        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y + mouseInput.x, 0f);
+        //transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y + mouseInput.x, 0f);
         cameraPitch -= mouseInput.y;
         cameraPitch = Mathf.Clamp(cameraPitch, -75f, 75f);
         camTrans.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
@@ -99,50 +107,57 @@ public class PlayerController : MonoBehaviour
         direction = moveInput;
         direction.y = 0;
 
+        float targetBlend = 0f;
+
         bool isMoving = direction.magnitude > 0.1f;
         bool isSprinting = Input.GetKey(KeyCode.LeftShift);
 
-        if (isMoving && !isSprinting)
+        if (!isMoving)
         {
-            canWalk = false;
-            canRun = true;
-            if (running != null)
-                running.Stop();
-            if (walking != null)
-                walking.Play();
-            if (anim != null)
-            {
-                anim.SetBool("walk", true);
-                anim.SetBool("run", false);
-            }
+            targetBlend = 0f; 
         }
-
+        else if (isMoving && !isSprinting)
+        {
+            targetBlend = 0.5f;
+        }
         else if (isMoving && isSprinting)
         {
-            canWalk = true;
-            canRun = false;
-            if (walking != null)
-                walking.Stop();
-            if (running != null)
-                running.Play();
-            if (anim != null)
-            {
-                anim.SetBool("walk", false);
-                anim.SetBool("run", true);
-            }
+            targetBlend = 1f;   
+        }
+
+        float currentBlend = anim.GetFloat("Blend");
+        float smoothBlend = Mathf.Lerp(currentBlend, targetBlend, Time.deltaTime * 8f);
+
+        if (!isAttacking)
+        {
+            anim.SetFloat("Blend", smoothBlend);
+        }
+
+        if (smoothBlend > 0.7f)
+        {
+            if (walking) walking.Stop();
+            if (running && !running.isPlaying) running.Play();
+        }
+        else if (smoothBlend > 0.1f)
+        {
+            if (running) running.Stop();
+            if (walking && !walking.isPlaying) walking.Play();
         }
         else
         {
-            canWalk = true;
-            canRun = true;
-            if (running != null)
-                running.Stop();
-            if (walking != null)
-                walking.Stop();
-            if (anim != null)
+            if (walking) walking.Stop();
+            if (running) running.Stop();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isAttacking)
             {
-                anim.SetBool("walk", false);
-                anim.SetBool("run", false);
+                StartCoroutine(DoPunch());
+            }
+            else
+            {
+                inputBuffered = true;
             }
         }
     }
@@ -152,9 +167,67 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    IEnumerator DoPunch()
+    {
+        isAttacking = true;
+        inputBuffered = false;
 
+        comboIndex = (comboIndex % totalComboCount) + 1;
+        string animName = "Box" + comboIndex;
 
+        anim.SetInteger("comboIndex", comboIndex);
+        anim.CrossFade(animName, 0.15f);
 
+        if (CC != null)
+            //CC.enabled = false;
 
+        while (!anim.GetCurrentAnimatorStateInfo(0).IsName(animName))
+            yield return null;
+
+        float hitStart = 0.25f;
+        float hitEnd = 0.45f;
+
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < hitStart)
+            yield return null;
+
+        //EnableDamage(comboIndex, true);
+
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < hitEnd)
+            yield return null;
+
+        //EnableDamage(comboIndex, false);
+
+        float timer = comboInputWindow;
+        while (timer > 0f)
+        {
+            if (inputBuffered)
+            {
+                StartCoroutine(DoPunch());
+                yield break;
+            }
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+
+        comboIndex = 0;
+        anim.CrossFade("MovementTree", 0.15f);
+
+        if (CC != null)
+            CC.enabled = true;
+
+        isAttacking = false;
+    }
+
+    /*
+    void EnableDamage(int index, bool enable)
+    {
+        for (int i = 0; i < punchDamages.Length; i++)
+        {
+            punchDamages[i].enable = (i == index - 1) && enable;
+            punchDamages[i].attacked = false;
+        }
+    }
+
+    */
 
 }
