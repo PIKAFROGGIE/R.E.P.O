3faee4d886414b85c09558e102ab0a3c697d4802
PIKACHU <pikaffroggie@gmail.com>
2026-01-08 22:43:28 +0800

    push player setup

--- Assets/Joshua/Scripts/PlayerSetup.cs
+++ Assets/Joshua/Scripts/PlayerSetup.cs
@@ -6,9 +6,6 @@ using UnityEngine.UI;
 
 public class PlayerSetup : MonoBehaviourPunCallbacks
 {
-    public GameObject[] FPS_Hands_ChildGameobjects;
-    public GameObject[] Soldier_ChildGameobjects;
-
     public Renderer BodyRenderer;
 
     public GameObject playerUIPrefab;
@@ -22,37 +19,25 @@ public class PlayerSetup : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
         playerController = GetComponent<PlayerController>();
 
         if (photonView.IsMine)
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-            foreach (GameObject gameObject in Soldier_ChildGameobjects)
-            {
-                gameObject.SetActive(false);
-            }
             if (playerUIPrefab != null)
             {
                 GameObject playerUIGameobject = Instantiate(playerUIPrefab);
             }
-            FPSCamera.enabled = true;
-            if(animator != null)
-            animator.SetBool("IsSoldier", false);
+           
         }
         else
         {
-            foreach (GameObject gameObject in Soldier_ChildGameobjects)
-            {
-                gameObject.SetActive(true);
-            }
-
-            playerController.enabled = false;
-            foreach (Camera cam in GetComponentsInChildren<Camera>())
+            foreach (Camera cam in playerController.GetComponentsInChildren<Camera>())
             {
                 cam.enabled = false;
             }
+            playerController.enabled = false;
         }
     }
 
