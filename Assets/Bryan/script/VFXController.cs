dbc2224ea9b3aea908046f4991f8fe9bf7b5717c
braiiyann <bryanllaw.04@gmail.com>
2026-01-10 01:48:34 +0800

    Update stupid error

--- Assets/Bryan/script/VFXController.cs
+++ Assets/Bryan/script/VFXController.cs
@@ -16,11 +16,6 @@ public class VFXController : MonoBehaviour
 
     void Awake()
     {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
         Instance = this;
     }
 
