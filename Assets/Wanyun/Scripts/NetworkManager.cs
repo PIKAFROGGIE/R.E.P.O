0023200ad57abd471c0be684e030d26f4a051866
braiiyann <bryanllaw.04@gmail.com>
2026-01-10 01:12:51 +0800

    NetworkManager

--- Assets/Wanyun/Scripts/NetworkManager.cs
+++ Assets/Wanyun/Scripts/NetworkManager.cs
@@ -129,7 +129,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.LoadLevel("Bryan");
+            PhotonNetwork.LoadLevel("LoadingScene");
         }
     }
     #endregion
